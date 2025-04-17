96d8a90ffdba63b4a6a7ee2ffbe26eef866181af
Tomasz Srebniak <275987@student.pwr.edu.pl>
2025-04-17 22:21:39 +0200

    Lista 2 - cz. 2

--- sprawozdanie2/Lista2.R
+++ sprawozdanie2/Lista2.R
@@ -37,3 +37,55 @@ vec <- as.numeric(vec$n)
 
 chi_sq_test(vec, rep(1/5, 5), method='pearson')  
 chi_sq_test(vec, rep(1/5, 5), method='IW')
+
+# zad 4
+# fisher.test
+
+# zad 5
+contingency_table <- table(ankieta$PŁEĆ, ankieta$CZY_KIER)
+fisher.test(contingency_table, conf.level = 0.95)
+# p-value > 0.05 => cechy są niezależne, prawdopodobieństwa są równe
+
+# zad 6
+ankieta['WIEK_KAT'] <- ifelse(ankieta$WIEK < 36, 'młody', ifelse(ankieta$WIEK < 46, 'średni', ifelse(ankieta$WIEK < 56, 'starszy', 'emerytura')))
+ankieta['CZY_ZADOW'] <- ifelse(ankieta$PYT_2 %in% c(1, 2), 'zadowolony', 'niezadowolony')
+# a)
+t <- table(ankieta$CZY_KIER, ankieta$WIEK_KAT)
+fisher.test(t, conf.level = 0.95) 
+# p-value > 0.05 => zajmowanie stanowiska kierowniczego nie zależy do wieku
+# b)
+t <- table(ankieta$CZY_KIER, ankieta$STAŻ)
+fisher.test(t, conf.level = 0.95)
+# p-value < 0.05 => zajmowanie stanowiska kierowniczego zależy od stażu pracy
+# c)
+t <- table(ankieta$PYT_2, ankieta$CZY_KIER)
+fisher.test(t, conf.level = 0.95)
+# p-value < 0.05 => zadowolenie ze szkoleń zależy od zajmowanego stanowiska
+# d)
+t <- table(ankieta$PYT_2, ankieta$STAŻ)
+fisher.test(t, conf.level = 0.95)
+# p-value < 0.05 => zadowolenie ze szkoleń zależy od stażu pracy
+# e)
+t <- table(ankieta$PYT_2, ankieta$PŁEĆ)
+fisher.test(t, conf.level = 0.95)
+# p-value > 0.05 => zadowolenie ze szkoleń nie zależy od płci
+# f)
+t <- table(ankieta$PYT_2, ankieta$WIEK_KAT)
+fisher.test(t, conf.level = 0.95, workspace = 2e7)
+# p-value > 0.05 => zadowolenie ze szkoleń nie zależy od wieku
+# c)'
+t <- table(ankieta$CZY_ZADOW, ankieta$CZY_KIER)
+fisher.test(t, conf.level = 0.95)
+# p-value > 0.05 => ogólniejsze zadowolenie ze szkoleń nie zależy od zajmowanego stanowiska
+# d)'
+t <- table(ankieta$CZY_ZADOW, ankieta$STAŻ)
+fisher.test(t, conf.level = 0.95)
+# p-value > 0.05 => ogólniejsze zadowolenie ze szkoleń nie zależy od stażu pracy
+# e)'
+t <- table(ankieta$CZY_ZADOW, ankieta$PŁEĆ)
+fisher.test(t, conf.level = 0.95)
+# p-value > 0.05 => ogólniejsze zadowolenie ze szkoleń nie zależy od płci
+# f)'
+t <- table(ankieta$CZY_ZADOW, ankieta$WIEK_KAT)
+fisher.test(t, conf.level = 0.95)
+# p-value > 0.05 => ogólniejsze zadowolenie ze szkoleń nie zależy od wieku
